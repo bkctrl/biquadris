ab5135134f5b32efd5f77521df1aa32768f03f26
bkctrl <hb3kang@uwaterloo.ca>
2024-04-02 05:19:13 -0400

    wrote to Block.h/.cc, added a param ctor in cell.h

--- cell.h
+++ cell.h
@@ -5,13 +5,14 @@
 #include "Observer.h"
 #include "subject.h"
 
-class Cell : public Observer , public Subject{
+class Cell : public Observer , public Subject {
     bool isOn;
     std::vector<Observer*> observers; 
     int r, c; // Row and column
 
 public:
     Cell();
+    Cell(int r, int c); // parametric ctor (ADDED WITH BLOCK BRANCH)
     bool getType() const; // 
     int getRow() const; // Returns the row number
     int getCol() const; // Returns the column number
