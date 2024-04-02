b807d387a530dfe194bcfaa9010d1f6bf6165f40
bkctrl <hb3kang@uwaterloo.ca>
2024-04-02 17:26:39 -0400

    push

--- main.cc
+++ main.cc
@@ -1,5 +1,32 @@
+#include <iostream>
+#include <string>
+#include "grid.h"
+#include "ComInterpreter.h"
+using namespace std;
 
-
-// handles user input 
-// handles level 
-// handles file input 
+int main(const int argc, const char* argv[]) {
+  // initialize grid and command interpreter
+  Grid grid;
+  CommandInterpreter cmdInterpreter(&grid); 
+  // command-line interface
+  for (int i = 1; i < argc; ++i) {
+    if (string(argv[i]) == "-text") {
+      //  runs the program in text-only mode
+    } else if (string(argv[i]) == "-seed") {
+      //  sets the random number generator’s seed to 
+    } else if (string(argv[i]) == "-scriptfile1") {
+      // Uses argv[i+1] instead of sequence1.txt as a source of blocks
+      //  for level 0, for player 1
+    } else if (string(argv[i]) == "-scriptfile2") {
+      // Uses argv[i+1] instead of sequence2.txt as a source of blocks
+      //  for level 0, for player 1
+    } else if (string(argv[i]) == "-startlevel") {
+      // Starts the game in level argv[i+1]
+    }
+  }
+  string user_input;
+  while (cin >> user_input) {
+    interpreter.interpretCommand(user_input);
+    grid.display();
+  }
+}
