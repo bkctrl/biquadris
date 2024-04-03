175def1e7a078f2969285ce879d363b6e00d9289
bkctrl <hb3kang@uwaterloo.ca>
2024-04-02 22:21:29 -0400

    cell rough

---
+++ cell.cc
@@ -0,0 +1,44 @@
+#include "Cell.h"
+
+Cell::Cell() : letter(""), r(0), c(0) {}
+
+Cell::Cell(int row, int col) : letter(""), r(row), c(col) {}
+
+Cell::~Cell() {}
+
+void Cell::setLetter(string c) {
+  letter = c;
+  update();
+}
+
+string Cell::getLetter() {
+  return letter;
+}
+
+int Cell::getRow() const {
+  return r;
+}
+
+int Cell::getCol() const {
+  return c;
+}
+
+bool Cell::isFilled() {
+  if (letter == "") {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+void Cell::notifyAll(Cell &c) {
+  for (auto it : observers) {
+    it->update(); 
+  }
+}
+
+void Cell::setCoords(int row, int col) {
+  r = row;
+  c = col;
+  update();
+}
